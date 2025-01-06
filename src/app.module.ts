3b21737e66b0e5f47f66a7b7fc37eb2fc4ed200d
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-06 20:58:03 -0300

    :rocket: feat: Add AppConfigModule with global configuration support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { WeatherModule } from './weather/weather.module';
+import { AppConfigModule } from './app-config/app-config.module';
 
 @Module({
-  imports: [WeatherModule],
+  imports: [WeatherModule, AppConfigModule],
   controllers: [AppController],
 })
 export class AppModule {}
