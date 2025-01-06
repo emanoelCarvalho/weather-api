607b097e354368351a7e7d8902112ce169c476d2
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-06 20:54:38 -0300

    :recycle: chore: Remove unused methods from WeatherService class

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -4,23 +4,5 @@ import { UpdateWeatherDto } from './dto/update-weather.dto';
 
 @Injectable()
 export class WeatherService {
-  create(createWeatherDto: CreateWeatherDto) {
-    return 'This action adds a new weather';
-  }
-
-  findAll() {
-    return `This action returns all weather`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} weather`;
-  }
-
-  update(id: number, updateWeatherDto: UpdateWeatherDto) {
-    return `This action updates a #${id} weather`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} weather`;
-  }
+  
 }
