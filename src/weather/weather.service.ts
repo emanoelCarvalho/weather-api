7887c665356782c8e0224d6685c8dedfbdc3d293
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-06 21:05:24 -0300

    :sparkles: feat: Integrate AppConfigService into WeatherService for dynamic API key retrieval

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -1,6 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { AppConfigService } from 'src/app-config/app-config.service';
 @Injectable()
 export class WeatherService {
-  private  readonly apiKey
+  constructor(private readonly configService: AppConfigService) {}
+
+  private readonly apiKey = this.configService.getApiKey
+  private readonly apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+  async getWeather(city: string): Promise<any> {
+    try {
+      const response = await axios.get(this.apiUrl, {
+        params: {
+          q: city, 
+          appid: this.apiKey, 
+          units: 'metric'
+        }, 
+      }); 
+      return response.data
+    } catch (error) {
+      throw new Error("Error fetching weather data.")
+    }
+  }
 }
