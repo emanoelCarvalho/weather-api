d29d6703c6b10d01767de8fd54873382cacdb5ab
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-06 21:11:36 -0300

    :fire: chore: Remove unused AppController and its reference from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { WeatherModule } from './weather/weather.module';
 import { AppConfigModule } from './app-config/app-config.module';
 
 @Module({
   imports: [WeatherModule, AppConfigModule],
-  controllers: [AppController],
 })
 export class AppModule {}
