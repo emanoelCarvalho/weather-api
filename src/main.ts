e8418952fd42662220dace137cb28c4eabb0723b
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-06 21:13:12 -0300

    :sparkles: feat: Update main.ts to use dynamic port retrieval from AppConfigService

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppConfigService } from './app-config/app-config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(AppConfigService);
+  const port = configService.getPort;
+  await app.listen(port);
 }
 bootstrap();
