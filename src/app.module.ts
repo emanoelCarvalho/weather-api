caa80e90f7e4118386df3ac0d9ded084ee925b79
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-13 22:21:12 -0300

    :sparkles: feat: Integrate Swagger module into the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { WeatherModule } from './weather/weather.module';
 import { AppConfigModule } from './app-config/app-config.module';
+import { SwaggerModule } from './swagger/swagger.module';
 
 @Module({
-  imports: [WeatherModule, AppConfigModule],
+  imports: [WeatherModule, AppConfigModule, SwaggerModule],
 })
 export class AppModule {}
