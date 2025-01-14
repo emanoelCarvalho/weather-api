a2765ea0384fc74eed6c72410063ce9ceb559a95
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-13 22:03:49 -0300

    fix: Correct variable name for port in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppConfigService } from './app-config/app-config.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(AppConfigService);
-  const port = configService.getPort;
-  await app.listen(port);
+  const PORT = configService.getPort;
+  await app.listen(PORT);
 }
 bootstrap();
