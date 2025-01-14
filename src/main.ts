4ef4f8181181049f716a82b278d701b1fbdd480c
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-13 22:25:31 -0300

    :sparkles: feat: Set up Swagger module configuration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './app-config/app-config.service';
+import { SwaggerModuleConfig } from './swagger/swagger.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
   const configService = app.get(AppConfigService);
   const PORT = configService.getPort;
+
+  SwaggerModuleConfig.setup(app);
 
   await app.listen(PORT);
 }
