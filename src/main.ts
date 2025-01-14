1250cbbcfd8ce5a5410cddcf2599e12e6501715f
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-13 22:05:55 -0300

    :sparkles: feat: Add Swagger support and update package dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './app-config/app-config.service';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Weather API')
+  .setDescription('Weather API description for developers')
+  .setVersion('1.0')
+  .build();
   const configService = app.get(AppConfigService);
   const PORT = configService.getPort;
+  
   await app.listen(PORT);
 }
 bootstrap();
