f3d73691479a88ab7292a203e3d8fb8275319fc0
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-13 22:21:29 -0300

    :recycle: refactor: Remove Swagger setup code from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './app-config/app-config.service';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Weather API')
-    .setDescription('Weather API description for developers')
-    .setVersion('1.0')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
   const configService = app.get(AppConfigService);
   const PORT = configService.getPort;
