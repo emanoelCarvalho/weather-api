5e83eb2210f37afe59f2a34592eb728049e1fb72
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-13 22:08:17 -0300

    :recycle: refactor: Improve formatting in main.ts and add Swagger annotations in WeatherController

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,16 @@ import { DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const config = new DocumentBuilder()
-  .setTitle('Weather API')
-  .setDescription('Weather API description for developers')
-  .setVersion('1.0')
-  .build();
+    .setTitle('Weather API')
+    .setDescription('Weather API description for developers')
+    .setVersion('1.0')
+    .build();
+
   const configService = app.get(AppConfigService);
   const PORT = configService.getPort;
-  
+
   await app.listen(PORT);
 }
 bootstrap();
--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { WeatherService } from './weather.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('weather')
 @Controller('weather')
 export class WeatherController {
   constructor(private readonly weatherService: WeatherService) {}
 
   @Get(':city')
+  @ApiOperation({ summary: 'Get weather by city' })
+  @ApiResponse({
+    
+  })
   async getWeather(@Param('city') city: string) {
     return this.weatherService.getWeather(city);
   }
