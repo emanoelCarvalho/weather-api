72f6e4d1839a7647b6dafbb52bb8b535819ce900
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-02-04 20:02:08 -0300

    :bug: fix: Refactor AppConfigService methods for consistency and readability

--- src/env/env.service.ts
+++ src/env/env.service.ts
@@ -3,19 +3,17 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppConfigService {
-    constructor(private readonly config: ConfigService) {
+  constructor(private readonly config: ConfigService) {}
 
-    }
+  get getPort(): number {
+    return this.config.get<number>('PORT');
+  }
 
-    get getPort(): number {
-        return this.config.get<number>('PORT');
-    }
+  get getApiKey(): string {
+    return this.config.get<string>('OPENWEATHERMAP_API_KEY');
+  }
 
-    get getApiKey(): string {
-        return this.config.get<string>('OPENWEATHERMAP_API_KEY')
-    }
-
-    get getApiUrl(): string {
-        return this.config.get<string>('OPENWEARTHERMAP_API_URL')
-    }
+  get getApiUrl(): string {
+    return this.config.get<string>('OPENWEARTHERMAP_API_URL');
+  }
 }
